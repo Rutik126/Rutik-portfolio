bdde00fe45babf3a6b9b8d09087645f9d1e9db00
Rutik Kumbhar <128617896+Rutik126@users.noreply.github.com>
2024-11-17 16:50:22 +0530

    Massage sender add

--- script.js
+++ script.js
@@ -84,6 +84,32 @@ function myMenuFunction(){
   
   srRight.reveal('.skills-box',{delay: 100})
   srRight.reveal('.form-control',{delay: 100})
+
+
+  jQuery(document).ready(function($) {
+    $(".mwb-form-control").focus(function(){
+      var tmpThis = $(this).val();
+      if(tmpThis == '' ) {
+        $(this).parent(".mwb-form-group").addClass("focus-input");
+      }
+      else if(tmpThis !='' ){
+        $(this).parent(".mwb-form-group").addClass("focus-input");
+      }
+    });
+    $(".mwb-form-control").blur(function(){
+      var tmpThis = $(this).val();
+      if(tmpThis == '' ) {
+        $(this).parent(".mwb-form-group").removeClass("focus-input");
+        $(this).siblings('.mwb-form-error').slideDown("3000");
+      }
+      else if(tmpThis !='' ){
+        $(this).parent(".mwb-form-group").addClass("focus-input");
+        $(this).siblings('.mwb-form-error').slideUp("3000");
+        
+      }
+    });
+    
+  }); 
   
   /* ----- CHANGE ACTIVE LINK ----- */
   
